ea4041ee539a264b8c55137e93ae7163072c5e76
josuecarias <josuecarias120401@gmail.com>
2024-10-17 21:52:53 -0600

    Version final del programa

--- main.cpp
+++ main.cpp
@@ -9,10 +9,11 @@
 
 using namespace std;
 
-// Variables para las rutas de los archivos
-string CTASMONETARIAS="E:/Mariano/Segundo Semestre/Algoritmos/Proyecto final/Proyecto-algoritmos-Umg/CTASMONETARIAS.txt";
-string TRANSACCIONES="E:/Mariano/Segundo Semestre/Algoritmos/Proyecto final/Proyecto-algoritmos-Umg/TRANSACCIONES.txt";
-string TEMP="E:/Mariano/Segundo Semestre/Algoritmos/Proyecto final/Proyecto-algoritmos-Umg/TEMP.txt";
+// Variables para las rutas de los archivos (uso de rutas relativas)
+string CTASMONETARIAS = "CTASMONETARIAS.txt";
+string TRANSACCIONES = "TRANSACCIONES.txt";
+string TEMP = "TEMP.txt";
+
 
 // Función para mostrar el menú
 char menu() {
@@ -30,6 +31,7 @@ char menu() {
     return opcion;
 }
 
+
 // Función para obtener la fecha y hora actuales
 string obtenerFechaHoraActual() {
     time_t ahora = time(0);
@@ -44,6 +46,114 @@ string obtenerFechaHoraActual() {
     return fechaHora.str();
 }
 
+// opcion 1 del menu 
+// funcion para crear cuentas
+string crearCuentaMonetaria() {
+    ofstream cuentas(CTASMONETARIAS, ios::app);  
+    if (!cuentas.is_open()) {
+        return "Error: No se pudo abrir el archivo de cuentas.";
+    }
+
+    string numeroCuenta, nombre, telefono, correo, saldoInicial;
+
+    cout << "Ingrese el numero de cuenta: ";
+    cin >> numeroCuenta;
+    cout << "Ingrese el nombre del titular: ";
+    cin.ignore(); 
+    getline(cin, nombre);
+    cout << "Ingrese el telefono del titular: ";
+    getline(cin, telefono);
+    cout << "Ingrese el correo del titular: ";
+    getline(cin, correo);
+    cout << "Ingrese el saldo inicial: ";
+    cin >> saldoInicial;
+
+    cuentas << numeroCuenta << ";" << nombre << ";" << telefono << ";" << correo << ";" << saldoInicial << endl;
+    cuentas.close();
+
+    return "Cuenta creada exitosamente.";
+}
+
+// opcion 2 del menu 
+// Función para operar depósitos
+string depositos() {
+    string numeroCuenta, linea, monto, nuevoSaldo, fecha;
+    bool cuentaEncontrada = false;
+
+    ifstream cuentas(CTASMONETARIAS);
+    if (!cuentas.is_open()) {
+        return "Error: No se pudo abrir el archivo de cuentas.";
+    }
+
+    ofstream temp(TEMP);
+    if (!temp.is_open()) {
+        cuentas.close();
+        return "Error: No se pudo crear el archivo temporal.";
+    }
+
+    cout << "Ingrese el numero de cuenta: ";
+    cin >> numeroCuenta;
+
+    while (getline(cuentas, linea)) {
+        stringstream ss(linea);
+        string cuenta, nombre, telefono, correo, saldo;
+
+        getline(ss, cuenta, ';');
+        getline(ss, nombre, ';');
+        getline(ss, telefono, ';');
+        getline(ss, correo, ';');
+        getline(ss, saldo, ';');
+
+        if (cuenta == numeroCuenta) {
+            cuentaEncontrada = true;
+            cout << "Cuenta encontrada: " << nombre << endl;
+            cout << "Saldo actual: " << saldo << endl;
+
+            cout << "Ingrese la cantidad a depositar: ";
+            cin >> monto;
+
+            try {
+                nuevoSaldo = to_string(stoi(saldo) + stoi(monto));
+                cout << "Depósito exitoso. Nuevo saldo: " << nuevoSaldo << endl;
+                temp << cuenta << ";" << nombre << ";" << telefono << ";" << correo << ";" << nuevoSaldo << endl;
+            } catch (invalid_argument &e) {
+                cout << "Error: Datos no válidos en el archivo de cuentas." << endl;
+                temp << linea << endl;
+            }
+        } else {
+            temp << linea << endl;
+        }
+    }
+
+    cuentas.close();
+    temp.close();
+
+    if (cuentaEncontrada) {
+        if (remove(CTASMONETARIAS.c_str()) != 0) {
+            return "Error al eliminar el archivo original.";
+        }
+
+        if (rename(TEMP.c_str(), CTASMONETARIAS.c_str()) != 0) {
+            return "Error al renombrar archivo temporal.";
+        }
+
+        ofstream transacciones(TRANSACCIONES, ios::app);
+        if (!transacciones.is_open()) {
+            return "Error al registrar transaccion.";
+        }
+
+        fecha = obtenerFechaHoraActual();
+        transacciones << numeroCuenta << ";" << fecha << ";" << monto << ";D" << endl;
+        transacciones.close();
+
+        return "Depósito registrado correctamente.";
+    } else {
+        remove(TEMP.c_str());
+        return "Cuenta no encontrada.";
+    }
+}
+
+//Opcion 3 del menu
 // Función para operar retiros
 string retiros() {
     string numeroCuenta, linea, monto, nuevoSaldo, fecha;
@@ -126,6 +236,96 @@ string retiros() {
         return "Cuenta no encontrada.";
     }
 }
+
+//Opcion 4 del menu
+// Función para operar transferencias
+string transferencias() {
+    string cuentaOrigen, cuentaDestino, linea, monto, nuevoSaldoOrigen, nuevoSaldoDestino;
+    bool cuentaOrigenEncontrada = false, cuentaDestinoEncontrada = false;
+    string saldoOrigen, saldoDestino;
+
+    ifstream cuentas(CTASMONETARIAS);
+    if (!cuentas.is_open()) {
+        return "Error: No se pudo abrir el archivo de cuentas.";
+    }
+
+    ofstream temp(TEMP);
+    if (!temp.is_open()) {
+        cuentas.close();
+        return "Error: No se pudo crear el archivo temporal.";
+    }
+
+    cout << "Ingrese el numero de cuenta origen: ";
+    cin >> cuentaOrigen;
+    cout << "Ingrese el numero de cuenta destino: ";
+    cin >> cuentaDestino;
+    cout << "Ingrese el monto a transferir: ";
+    cin >> monto;
+
+    while (getline(cuentas, linea)) {
+        stringstream ss(linea);
+        string cuenta, nombre, telefono, correo, saldo;
+
+        getline(ss, cuenta, ';');
+        getline(ss, nombre, ';');
+        getline(ss, telefono, ';');
+        getline(ss, correo, ';');
+        getline(ss, saldo, ';');
+
+        // Actualizar la cuenta de origen
+        if (cuenta == cuentaOrigen) {
+            cuentaOrigenEncontrada = true;
+            saldoOrigen = saldo;
+            if (stoi(saldoOrigen) >= stoi(monto)) {
+                nuevoSaldoOrigen = to_string(stoi(saldoOrigen) - stoi(monto));
+                temp << cuenta << ";" << nombre << ";" << telefono << ";" << correo << ";" << nuevoSaldoOrigen << endl;
+            } else {
+                cout << "Fondos insuficientes en la cuenta de origen." << endl;
+                temp << linea << endl;
+            }
+        } 
+        // Actualizar la cuenta de destino
+        else if (cuenta == cuentaDestino) {
+            cuentaDestinoEncontrada = true;
+            saldoDestino = saldo;
+            nuevoSaldoDestino = to_string(stoi(saldoDestino) + stoi(monto));
+            temp << cuenta << ";" << nombre << ";" << telefono << ";" << correo << ";" << nuevoSaldoDestino << endl;
+        } 
+        else {
+            temp << linea << endl;
+        }
+    }
+
+    cuentas.close();
+    temp.close();
+
+    if (cuentaOrigenEncontrada && cuentaDestinoEncontrada) {
+        if (remove(CTASMONETARIAS.c_str()) != 0) {
+            return "Error al eliminar el archivo original.";
+        }
+
+        if (rename(TEMP.c_str(), CTASMONETARIAS.c_str()) != 0) {
+            return "Error al renombrar archivo temporal.";
+        }
+
+        ofstream transacciones(TRANSACCIONES, ios::app);
+        if (!transacciones.is_open()) {
+            return "Error al registrar transacción.";
+        }
+
+        string fecha = obtenerFechaHoraActual();
+        transacciones << cuentaOrigen << ";" << fecha << ";" << monto << ";T-D" << endl;  
+        transacciones << cuentaDestino << ";" << fecha << ";" << monto << ";T-C" << endl; 
+        transacciones.close();
+
+        return "Transferencia registrada correctamente.";
+    } else {
+        remove(TEMP.c_str());
+        return "Error: Cuenta origen o destino no encontrada.";
+    }
+}
+
+//Opcion 5 del menu
 // Función para consultar el saldo de una cuenta
 string consultaSaldo() {
     string numeroCuenta, linea;
@@ -174,10 +374,12 @@ int main() {
         switch (opcion) {
             case '1':
                 cout << "Has seleccionado: Crear cuentas monetarias" << endl;
+                cout << crearCuentaMonetaria() << endl;
                 cout << "--------------------------------------" << endl;
                 break;
             case '2':
                 cout << "Has seleccionado: Operar depositos" << endl;
+                cout << depositos() << endl;
                 cout << "--------------------------------------" << endl;
                 break;
             case '3':
@@ -187,6 +389,7 @@ int main() {
                 break;
             case '4':
                 cout << "Has seleccionado: Transferencias" << endl;
+                cout << transferencias() << endl;
                 cout << "--------------------------------------" << endl;
                 break;
             case '5':
